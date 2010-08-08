1e3ba82b82c92e99c2011b9386ce65dfc3a21090
Hong Jiang <hjiang@onyx-international.com>
2010-08-08 20:01:27 +0800

    Fix cmake script bugs: do not use environment variables to control flow.

--- arm_toolchain.cmake
+++ arm_toolchain.cmake
@@ -1,25 +1,17 @@
 # Define arm tool chain
 MACRO (USE_ARM_TOOLCHAIN)
     MESSAGE("Use arm toolchain.")
-    list(LENGTH "$ENV{CC}" cc_length)
-    list(LENGTH "$ENV{CXX}" cxx_length)
-
-    INCLUDE(CMakeForceCompiler)
-    if (${cc_length} EQUAL 0)
-        CMAKE_FORCE_C_COMPILER(arm-linux-gcc GNU)
-    endif (${cc_length} EQUAL 0)
-    if (${cxx_length} EQUAL 0)
-        CMAKE_FORCE_CXX_COMPILER(arm-linux-g++ GNU)
-    endif (${cxx_length} EQUAL 0)
 
     # Define the dependency libraries root path.
-    if ($ENV{ONYX_SDK_ROOT})
-        message("Onyx SDK Root path $ENV{ONYX_SDK_ROOT}")
-        SET(CMAKE_FIND_ROOT_PATH $ENV{ONYX_SDK_ROOT})
-    else ($ENV{ONYX_SDK_ROOT})
+    if (ONYX_SDK_ROOT)
+        message("Onyx SDK Root path ${ONYX_SDK_ROOT}")
+        SET(CMAKE_FIND_ROOT_PATH ${ONYX_SDK_ROOT})
+        link_directories(${ONYX_SDK_ROOT}/lib)
+    else (ONYX_SDK_ROOT)
         message("Use default path: /opt/onyx/arm/")
         SET(CMAKE_FIND_ROOT_PATH "/opt/onyx/arm/")
-    endif ($ENV{ONYX_SDK_ROOT})
+        link_directories("/opt/onyx/arm/lib")
+    endif (ONYX_SDK_ROOT)
     SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
     if (${CMAKE_FIND_ROOT_PATH})
@@ -45,12 +37,5 @@ MACRO (USE_ARM_TOOLCHAIN)
     ADD_DEFINITIONS(-DBS80_INIT_MAIN)
     ADD_DEFINITIONS(-DBS97_INIT_MAIN)
     ADD_DEFINITIONS(-DSFM_M25P20)
-
-
-    IF($ENV{HAS_TOUCH})
-        ADD_DEFINITIONS(-DHAS_TOUCH)
-    ELSE ($ENV{HAS_TOUCH})
-        UNSET(HAS_TOUCH)
-    ENDIF ($ENV{HAS_TOUCH})
 
 ENDMACRO(USE_ARM_TOOLCHAIN)
